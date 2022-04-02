308f58a5df60bfb7ede8def68943cfc36b8957e9
sija <sijaribu12@gmail.com>
2022-04-02 20:48:07 +0300

    scrap code testing simpleFOC with ESP32 and stepper motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,76 @@
-#include <Arduino.h>
-#include "wifiManager.h"
-#include "otawebserver.h"
-// #include "mpu.h"
-// #include "filewrite.h"
-// #include "getvcc.h"
-#include "websocket.h"
-#include "detector.h"
-// #include "button.h"
-// #include "digitalmp.h"
-#include "i2cdevver.h"
-// #include "indicator.h"
-// #include "asynccudp.h"
-#include "streamer.h"
- 
-struct Button
-{
-  const uint8_t PIN;
-  bool pressed;
-};
+// Open loop motor control example
+ #include <SimpleFOC.h>
 
-Button button1 = {18, false};
 
-void IRAM_ATTR isr()
-{
-  button1.pressed = !button1.pressed;
-  //   redo();
-  Serial.println(button1.pressed);
-}
+// BLDC motor & driver instance
+// BLDCMotor motor = BLDCMotor(pole pair number);
+// BLDCMotor motor = BLDCMotor(11);
+// // BLDCDriver3PWM driver = BLDCDriver3PWM(pwmA, pwmB, pwmC, Enable(optional));
+// BLDCDriver3PWM driver = BLDCDriver3PWM(25, 26, 27, 14);
 
-bool wifiConnected = false;
+// Stepper motor & driver instance
+StepperMotor motor = StepperMotor(50);
+// // 25, 26, 27, 14,12,13
+// //  StepperDriver4PWM( int ph1A,int ph1B,int ph2A,int ph2B, int en1 (optional), int en2 (optional))
+// //  - ph1A, ph1B - phase 1 pwm pins
+// //  - ph2A, ph2B - phase 2 pwm pins
+// //  - en1, en2  - enable pins (optional input)
+// //27-in1,26-in2,14-in3,25-in4
+// StepperDriver4PWM driver = StepperDriver4PWM(25, 26, 27, 14,12,13);
+StepperDriver4PWM driver = StepperDriver4PWM(25, 26, 27, 14);
 
-void setup()
-{
-  // pinMode(5, OUTPUT);
-  // buttonsetup();
+//target variable
+float target_position = 0;
+
+// instantiate the commander
+Commander command = Commander(Serial);
+void doTarget(char* cmd) { command.scalar(&target_position, cmd); }
+void doLimit(char* cmd) { command.scalar(&motor.voltage_limit, cmd); }
+void doVelocity(char* cmd) { command.scalar(&motor.velocity_limit, cmd); }
+
+void setup() {
+
+  // driver config
+  // power supply voltage [V]
+  driver.voltage_power_supply = 12;
+  // limit the maximal dc voltage the driver can set
+  // as a protection measure for the low-resistance motors
+  // this value is fixed on startup
+  driver.voltage_limit = 6;
+  driver.init();
+  // link the motor and the driver
+  motor.linkDriver(&driver);
+
+  // limiting motor movements
+  // limit the voltage to be set to the motor
+  // start very low for high resistance motors
+  // currnet = resistance*voltage, so try to be well under 1Amp
+  motor.voltage_limit = 3;   // [V]
+  // limit/set the velocity of the transition in between 
+  // target angles
+  motor.velocity_limit = 5; // [rad/s] cca 50rpm
+  // open loop control config
+  motor.controller = MotionControlType::angle_openloop;
+
+  // init motor hardware
+  motor.init();
+
+  // add target command T
+  command.add('T', doTarget, "target angle");
+  command.add('L', doLimit, "voltage limit");
+  command.add('V', doLimit, "movement velocity");
+
   Serial.begin(115200);
-  connect2nearestAP();
-  checkWiFi();
-  OTAWebStart();
-  initWebSocket();
-  //  startMpu();
-  //  attachInterrupt(button1.PIN, isr, FALLING);
-  //  setupSpiffs();
-  delay(5000);
-  beginUdp();
-  //  lightsetup();
-  //  digitalmpsetup();
-  // setupUDP();
-  // dmpsetup();
-  camerasetup();
+  Serial.println("Motor ready!");
+  Serial.println("Set target position [rad]");
+  _delay(7000);
 }
 
-void loop()
-{
-  if (wifiConnected == false)
-  {
-    if (WiFi.status() == WL_CONNECTED)
-    {
-      Serial.println("WiFi is connected");
-      Serial.println(WiFi.localIP());
-      wifiConnected = true;
-    }
-  }
-  OTAWebServer();
-  // buttonloop();
-  // broadcaster();
-  rentol();
-  // lightloop();
-  // udpier();
-  // vcc_init();
-  //   redo();
-  // loopWOM();
-  // delay(1);
-  // digitalmploop();
-  // dmploop();
+void loop() {
+  // open  loop angle movements
+  // using motor.voltage_limit and motor.velocity_limit
+  motor.move(target_position);
+  
+  // user communication
+  command.run();
 }
